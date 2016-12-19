2457e3e00179ff86a84f4231b4fc62cc55d8f60f
Josech <xjosechx@gmail.com>
2016-12-19 00:00:07 -0500

    Mi perfil, imagen circular, transición

--- app/src/main/java/com/josech/platzigram/view/fragment/ProfileFragment.java
+++ app/src/main/java/com/josech/platzigram/view/fragment/ProfileFragment.java
@@ -2,12 +2,21 @@ package com.josech.platzigram.view.fragment;
 
 
 import android.os.Bundle;
+import android.support.design.widget.CollapsingToolbarLayout;
 import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.josech.platzigram.R;
+import com.josech.platzigram.adapter.PictureAdapterRecyclerView;
+import com.josech.platzigram.model.Picture;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -24,7 +33,35 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+        showToolbar("",false,view);
+
+        RecyclerView pictureRecycler = (RecyclerView) view.findViewById(R.id.pictureProfileRecycler);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
+
+        pictureRecycler.setLayoutManager(linearLayoutManager);
+        PictureAdapterRecyclerView pictureAdapterRecyclerView =
+                new PictureAdapterRecyclerView(buildPicture(), R.layout.cardview_picture, getActivity());
+        pictureRecycler.setAdapter(pictureAdapterRecyclerView);
+
+        return view;
+    }
+
+    public ArrayList<Picture> buildPicture(){
+        ArrayList<Picture> pictures = new ArrayList<>();
+        pictures.add(new Picture("https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcRrFbOzLzNanorC-ytElyeVL--id0UFeSY3yuVUgbpxCa-83Jr95w","Josech Martínez", "4 días", "3 Me Gusta"));
+        pictures.add(new Picture("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcRfyZb1waVxH2RVFeZzj-Fs85yP14PAlHVRpi9aISf3plDzpHl7","Jhon Martínez", "3 días", "2 Me Gusta"));
+        pictures.add(new Picture("https://encrypted-tbn2.gstatic.com/images?q=tbn:ANd9GcQh8yAeNcBt5BRDRO7JytNN-4QRc2zva3NXtjEG4FEqWPaNzjc9","Milena Pineda", "2 días", "1 Me Gusta"));
+        return pictures;
+    }
+
+    public void showToolbar(String tittle, boolean upButton, View view){
+        Toolbar toolbar = (Toolbar) view.findViewById(R.id.toolbar);
+        ((AppCompatActivity) getActivity()).setSupportActionBar(toolbar);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setTitle(tittle);
+        ((AppCompatActivity) getActivity()).getSupportActionBar().setDisplayHomeAsUpEnabled(upButton);
     }
 
 }
