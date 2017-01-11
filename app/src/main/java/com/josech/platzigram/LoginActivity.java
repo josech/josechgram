d34a3e8a39525d46cbf9eb3aa86a9439065cf153
Josech <xjosechx@gmail.com>
2017-01-11 14:25:51 -0500

    Peticiones Rest

--- app/src/main/java/com/josech/platzigram/LoginActivity.java
+++ app/src/main/java/com/josech/platzigram/LoginActivity.java
@@ -2,19 +2,85 @@ package com.josech.platzigram;
 
 import android.content.Intent;
 import android.net.Uri;
+import android.os.AsyncTask;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.JsonArrayRequest;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.josech.platzigram.view.ContainerActivity;
 import com.josech.platzigram.view.CreateAccountActivity;
 
-public class LoginActivity extends AppCompatActivity {
+import org.apache.http.HttpEntity;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.json.JSONArray;
+import org.json.JSONException;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+
+public class LoginActivity extends AppCompatActivity {
+    JSONArray jArrayObject;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        //new MyHttpAsyncTask().execute("https://jsonplaceholder.typicode.com/users");
+
+        String url ="https://jsonplaceholder.typicode.com/users";
+        // Instantiate the RequestQueue.
+        RequestQueue queue = Volley.newRequestQueue(this);
+        JsonArrayRequest jsonArrayRequest = new JsonArrayRequest(
+                Request.Method.GET,
+                url,
+                null,
+                new Response.Listener<JSONArray>(){
+                    @Override
+                    public void onResponse(JSONArray response) {
+                        Log.d("JSON", "Respuesta: " + response.toString());
+                    }
+                },
+                new Response.ErrorListener(){
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        Log.d("Error", "onErrorResponse: " + error.getMessage());
+                    }
+                }
+        );
+        queue.add(jsonArrayRequest);
+
+        /*
+        // Request a string response from the provided URL.
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Display the first 500 characters of the response string.
+                        Log.d("JSON", "respuesta: " + response.toString());//("Response is: "+ response.substring(0,500));
+                    }
+                }, new Response.ErrorListener() {
+                @Override
+                public void onErrorResponse(VolleyError error) {
+                    Log.d("ERROR", );
+                }
+        });
+        // Add the request to the RequestQueue.
+        queue.add(stringRequest);
+        */
     }
 
     public void goCreateAccount(View view){
@@ -31,5 +97,60 @@ public class LoginActivity extends AppCompatActivity {
         Intent intent = new Intent(Intent.ACTION_VIEW);
         intent.setData(Uri.parse("https://github.com/josech/josechgram"));
         startActivity(intent);
+    }
+
+    private class MyHttpAsyncTask extends AsyncTask<String, Void, String>{
+        InputStream is = null;
+        String json = "";
+        @Override
+        protected String doInBackground(String... urls) {
+            //Making HTTP request
+            String result = "";
+            try {
+                // defaultHttpClient
+                DefaultHttpClient httpClient = new DefaultHttpClient();
+                HttpGet httpget = new HttpGet(urls[0]);
+
+                HttpResponse httpResponse = httpClient.execute(httpget);
+                HttpEntity httpEntity = httpResponse.getEntity();
+                is = httpEntity.getContent();
+
+            } catch (UnsupportedEncodingException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+
+            try {
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        is, "iso-8859-1"), 8);
+                StringBuilder sb = new StringBuilder();
+                String line = null;
+                while ((line = reader.readLine()) != null) {
+                    sb.append(line + "\n");
+                }
+                is.close();
+                result = sb.toString();
+            } catch (Exception e) {
+                Log.e("Buffer Error", "Error converting result " + e.toString());
+            }
+
+            return result;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+            // AQUI VA EL PARSEO
+            try {
+                jArrayObject = new JSONArray(result);
+
+                //JSONObject jObject = new JSONObject(result);
+
+            } catch (JSONException e) {
+                Log.e("JSON Parser", "Error parsing data " + e.toString());
+            }
+        }
     }
 }
